c3fb31875f6c9a2c64b282434ea1d013f0efd089
gustavobadke <gustavobadke@gmail.com>
2025-09-26 15:41:30 -0300

    direto na tabela

--- src/br/com/gnexum/SapConnection.java
+++ src/br/com/gnexum/SapConnection.java
@@ -25,6 +25,32 @@ public class SapConnection {
 
             //System.out.println("Connected to SAP system: " + destination.getAttributes().getSystemID());
 
+            JCoFunction function = destination.getRepository().getFunction("J_1BNFDOC");
+            if (function == null) {
+                throw new RuntimeException("Função J_1BNFDOC não encontrada no SAP.");
+            }
+
+            // 1. Define os parâmetros de entrada (exemplo fictício)
+            JCoParameterList importParams = function.getImportParameterList();
+            importParams.setValue("DOCUMENT_NUMBER", "123456789");
+
+            // 2. Executa a função no SAP
+            function.execute(destination);
+
+            // 3. Lê parâmetros de saída (export)
+            JCoParameterList exportParams = function.getExportParameterList();
+            String status = exportParams.getString("STATUS");
+            System.out.println("Status da NF: " + status);
+
+            // 4. Se houver tabelas de retorno
+            JCoTable table = function.getTableParameterList().getTable("NFE_DATA");
+            for (int i = 0; i < table.getNumRows(); i++) {
+                table.setRow(i);
+                System.out.println("NF-e: " + table.getString("NFE_NUMBER"));
+                // Pegue outros campos conforme necessário
+            }
+
+
             JCoFunction testFunc = destination.getRepository().getFunction("STFC_CONNECTION");
             testFunc.execute(destination);
             System.out.println(testFunc.getExportParameterList().getString("ECHOTEXT"));
