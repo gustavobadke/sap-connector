cd246f69908dec7f1ba4915c752432f1b041b927
gustavobadke <gustavobadke@gmail.com>
2025-09-26 10:12:04 -0300

    teste de conexão

--- src/br/com/gnexum/SapConnection.java
+++ src/br/com/gnexum/SapConnection.java
@@ -3,12 +3,27 @@ package br.com.gnexum;
 import com.sap.conn.jco.JCoDestination;
 import com.sap.conn.jco.JCoDestinationManager;
 import com.sap.conn.jco.JCoException;
+import com.sap.conn.jco.JCoFunction;
 
 public class SapConnection {
     public static void main(String[] args) {
         try {
+            System.out.println("Attempting to connect to SAP...");
+
             JCoDestination destination = JCoDestinationManager.getDestination("SAP_CONFIG");
+
+            System.out.println("Connection successful!");
+
+            destination.ping();
+
+            System.out.println("Ping successful!");
+
             System.out.println("Connected to SAP system: " + destination.getAttributes().getSystemID());
+
+            JCoFunction testFunc = destination.getRepository().getFunction("STFC_CONNECTION");
+            testFunc.execute(destination);
+            System.out.println(testFunc.getExportParameterList().getString("ECHOTEXT"));
+            
         } catch (JCoException e) {
             e.printStackTrace();
         }
